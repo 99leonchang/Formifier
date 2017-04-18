4b42d824d414e2d363f747e01a792122933c7e52
Leon Chang <99leonchang@gmail.com>
2017-04-18 23:24:45 +0800

    Casting error fix

--- app/routes/api.js
+++ app/routes/api.js
@@ -12,7 +12,7 @@ var mongoose    = require('mongoose');
 var bcrypt      = require('bcrypt');
 var Form        = require('../models/forms');
 var User        = require('../models/user');
-
+app.set('superSecret', config.secret);
 //----- Authentication -----//
 exports.middleware = function(req, res, next) {
 
@@ -23,7 +23,7 @@ exports.middleware = function(req, res, next) {
     if (token) {
 
         // verifies secret and checks exp
-        jwt.verify(token, config.secret, function(err, decoded) {
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
             if (err) {
                 return res.json({ success: false, message: 'Failed to authenticate token.' });
             } else {
